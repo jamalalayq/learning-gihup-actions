fceb6c8c0f6dd5c0d844e2102ed0867ea19923fc
Jamal alayq <jamal.alayq@mongez.app>
2021-01-09 18:20:00 +0200

    Setup fastlane

---
+++ GithubActions/Shared/GithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsApp.swift
+//  Shared
+//
+//  Created by Jamal alayq on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubActions/Tests iOS/Tests_iOS.swift
@@ -0,0 +1,28 @@
+//
+//  Tests_iOS.swift
+//  Tests iOS
+//
+//  Created by Jamal alayq on 1/9/21.
+//
+
+import XCTest
+
+class Tests_iOS: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAddition_onePlusOne_Two() {
+        XCTAssertEqual(1 + 1, 2)
+    }
+}
